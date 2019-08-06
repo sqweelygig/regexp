002d46c36e9f1ab7a6bfb0ae94dd47f75db2d943
Andrew Lucas <sqweelygig@googlemail.com>
2019-08-06 14:40:20 +0100

    Simple flag grabber

--- src/regexp.ts
+++ src/regexp.ts
@@ -1,1 +1,27 @@
-export const hello = () => "Hello World!";
+export interface Flags {
+	dotAll: boolean;
+	global: boolean;
+	ignoreCase: boolean;
+	multiline: boolean;
+	unicode: boolean;
+}
+
+export class RegExp {
+	public static makeFlagsArray(flags: string): Flags {
+		return {
+			dotAll: flags.includes("s"),
+			global: flags.includes("g"),
+			ignoreCase: flags.includes("i"),
+			multiline: flags.includes("m"),
+			unicode: flags.includes("u")
+		}
+	}
+
+	public readonly flags: Flags;
+
+	public constructor(private readonly source: string) {
+		const flagString = source.split("/").pop();
+		this.flags = RegExp.makeFlagsArray(flagString);
+	}
+}
+
--- test/regexp.spec.ts
+++ test/regexp.spec.ts
@@ -1,10 +1,44 @@
 import { expect } from "chai";
 import "mocha";
-import { hello } from "../src/regexp";
+import { RegExp } from "../src/regexp";
 
-describe("Hello function", () => {
-	it("should return hello world", () => {
-		const result = hello();
-		expect(result).to.equal("Hello World!");
+/**
+ * Tests for the RegExp class.
+ * The NodeJS v12.x native implementation should be considered authoritative.
+ */
+
+describe("Flags Parser", () => {
+	it("should calculate an empty flags object", () => {
+		const regExp = new RegExp(/a/.toString());
+		const result = regExp.flags;
+		expect(result).to.deep.equal({
+			dotAll: false,
+			global: false,
+			ignoreCase: false,
+			multiline: false,
+			unicode: false,
+		});
+	});
+	it("should calculate a full flags object", () => {
+		const regExp = new RegExp(/a/igmsu.toString());
+		const result = regExp.flags;
+		expect(result).to.deep.equal({
+			dotAll: true,
+			global: true,
+			ignoreCase: true,
+			multiline: true,
+			unicode: true,
+		});
+	});
+	it("should calculate an example part set of flags", () => {
+		const regExp = new RegExp(/a/igm.toString());
+		const result = regExp.flags;
+		expect(result).to.deep.equal({
+			dotAll: false,
+			global: true,
+			ignoreCase: true,
+			multiline: true,
+			unicode: false,
+		});
 	});
 });
