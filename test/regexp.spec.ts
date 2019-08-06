1730c9ca7f1ea408576098e8fd6b361237957a21
Andrew Lucas <sqweelygig@googlemail.com>
2019-08-06 13:29:07 +0100

    Hello World!

---
+++ test/regexp.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import "mocha";
+import { hello } from "../src/regexp";
+
+describe("Hello function", () => {
+	it("should return hello world", () => {
+		const result = hello();
+		expect(result).to.equal("Hello World!");
+	});
+});
