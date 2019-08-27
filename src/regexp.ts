8277faee926dc74b02578b9128a2ff2243576cbd
Andrew Lucas <sqweelygig@googlemail.com>
2019-08-27 14:27:51 +0100

    Added comments to RegExp while it is fresh

--- src/regexp.ts
+++ src/regexp.ts
@@ -42,19 +42,26 @@ export class RegExp {
 
 	public static parseGroup(group: string): MatchGroup {
 		if (group.length === 0) {
+			// Match the null character, ie `[]` in NodeJS native implementation
 			return new MatchCharacter("\0");
 		} else if (group.length === 1) {
+			// Match a single character
 			return new MatchCharacter(group);
 		} else if (group.charAt(1) === "-" && group.length > 2) {
+			// Check if our group begins with a range, eg `a-z`
 			const matchHead = new MatchRange(group.charAt(0), group.charAt(2));
 			if (group.length === 3) {
+				// The group is only the range from above
 				return matchHead;
 			} else {
+				// Recurse upon the rest of the group, matching head or tail
 				const matchTail = RegExp.parseGroup(group.substring(3));
 				return new MatchEither(matchHead, matchTail);
 			}
 		} else {
+			// The first part of the group therefore must be a character
 			const matchHead = new MatchCharacter(group.charAt(0));
+			// Recurse upon the rest of the group, matching head or tail
 			const matchTail = RegExp.parseGroup(group.substring(1));
 			return new MatchEither(matchHead, matchTail);
 		}
@@ -66,19 +73,24 @@ export class RegExp {
 		const returnValue = [];
 		pattern.split("").forEach((character) => {
 			if (!groupMode && character === "[") {
+				// Switch into group mode
 				groupAccumulator = "";
 				groupMode = true;
 			} else if (groupMode && character === "]") {
 				groupMode = false;
 				if (groupAccumulator.charAt(0) === "^") {
+					// The group we gathered begins `^` so should be inverted
 					const baseMatch = RegExp.parseGroup(groupAccumulator.substring(1));
 					returnValue.push(new MatchInvert(baseMatch));
 				} else {
+					// Analyse the group we gathered
 					returnValue.push(RegExp.parseGroup(groupAccumulator));
 				}
 			} else if (groupMode) {
+				// Grow the group, if in group mode
 				groupAccumulator += character;
 			} else {
+				// We've a single character group, so can analyse that
 				returnValue.push(RegExp.parseGroup(character));
 			}
 		});
@@ -86,8 +98,11 @@ export class RegExp {
 	}
 
 	public static testMatch(haystack: string, needle: MatchGroup[]): boolean {
+		// If any part of the haystack matches ...
 		return haystack.split("").some((character, start) => {
+			// ... every part of the needle ...
 			return needle.every((matchGroup, offset) => {
+				// ... tested character-by-character
 				return (
 					haystack.length > start + offset &&
 					matchGroup.test(haystack.charAt(start + offset))
