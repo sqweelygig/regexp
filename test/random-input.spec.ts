cfd9f560f77bf1b748f7da08af645bf08482d806
Andrew Lucas <sqweelygig@googlemail.com>
2019-08-27 14:17:20 +0100

    Refactored random test file to be time limited rather than count limited

--- test/random-input.spec.ts
+++ test/random-input.spec.ts
@@ -8,59 +8,61 @@ import { RegExp as MyRegExp } from "../src/regexp";
  */
 describe("Random Input Testing", () => {
 	describe("RegExp", () => {
-		describe("public static testMatch(needle: MatchGroup[], haystack: string)", () => {
-			const haystackScope = "abc";
-			// TODO: Merge these once I have support for ^ meaning beginning of string
-			const startPatternScope = haystackScope + "[]-";
-			const midPatternScope = startPatternScope + "^";
-			const haystackChars = haystackScope.split("");
-			const startChars = startPatternScope.split("");
-			const midChars = midPatternScope.split("");
-			const approxTests = Math.pow(2, 12);
-			const maxPatternLength = Math.pow(2, 3);
-			for (let i = 0; i < Math.ceil(Math.sqrt(approxTests)); i++) {
-				let regExp;
-				let pattern;
-				const patternLength = Math.ceil(Math.random() * maxPatternLength);
-				while (regExp === undefined) {
-					const startIndex = Math.floor(Math.random() * startChars.length);
-					pattern = startChars[startIndex];
-					for (let j = 0; j < patternLength; j++) {
-						const midIndex = Math.floor(Math.random() * midChars.length);
-						pattern += midChars[midIndex];
-					}
-					try {
-						regExp = new RegExp(pattern, "");
-					} catch (error) {
-						/* Intentionally left blank, the loop tries a new pattern */
-					}
+		const endStackPushing = Date.now() + 50;
+		const haystackScope = "abc";
+		// TODO: Merge these once I have support for ^ meaning beginning of string
+		const startPatternScope = haystackScope + "[]-";
+		const midPatternScope = startPatternScope + "^";
+		const haystackChars = haystackScope.split("");
+		const startChars = startPatternScope.split("");
+		const midChars = midPatternScope.split("");
+		const maxPatternLength = Math.pow(2, 3);
+		while (Date.now() < endStackPushing) {
+			const patternLength = Math.floor(Math.random() * maxPatternLength);
+			let regExp;
+			let pattern;
+			// Build random patterns until a valid pattern is found
+			while (regExp === undefined) {
+				const startIndex = Math.floor(Math.random() * startChars.length);
+				pattern = startChars[startIndex];
+				for (let i = 0; i < patternLength; i++) {
+					const midIndex = Math.floor(Math.random() * midChars.length);
+					pattern += midChars[midIndex];
 				}
-				let myRegExp;
-				it(`should construct using /${pattern}/`, () => {
-					try {
-						myRegExp = new MyRegExp(`/${pattern}/`);
-					} catch (error) {
-						// tslint:disable-next-line:no-console
-						console.error(error);
-					}
-					expect(myRegExp).to.not.equal(undefined);
-				});
-				for (let k = 0; k < Math.floor(Math.sqrt(approxTests)); k++) {
-					let haystack = "";
-					const extraCharacters = Math.ceil(
-						Math.random() * maxPatternLength * 2,
-					);
-					const haystackLength = patternLength + extraCharacters;
-					for (let j = 0; j < haystackLength; j++) {
-						const index = Math.floor(Math.random() * haystackChars.length);
-						haystack += haystackChars[index];
-					}
-					const expectation = regExp.test(haystack);
-					it(`should return ${expectation} for /${pattern}/ in ${haystack}`, () => {
-						expect(myRegExp.test(haystack)).to.equal(expectation);
-					});
+				try {
+					regExp = new RegExp(pattern, "");
+				} catch (error) {
+					/* Intentionally left blank, the loop tries a new pattern */
 				}
 			}
-		});
+			describe(pattern, () => {
+				describe("public constructor(regExp: string)", () => {
+					it("should construct.", () => {
+						const myRegExp = new MyRegExp(`/${pattern}/`);
+						expect(myRegExp).to.not.equal(undefined);
+					});
+				});
+				describe("public static testMatch(needle: MatchGroup[], haystack: string)", () => {
+					const needle = MyRegExp.parsePattern(pattern);
+					for (let j = 0; j < 16; j++) {
+						const haystackPicks = [];
+						const haystackLength = Math.ceil(
+							Math.random() * maxPatternLength * 3,
+						);
+						for (let k = 0; k < haystackLength; k++) {
+							const index = Math.floor(Math.random() * haystackChars.length);
+							haystackPicks.push(haystackChars[index]);
+						}
+						const haystack = haystackPicks.join("");
+						const expectation = regExp.test(haystack);
+						const matches = expectation ? "matches" : "does not match";
+						it(`should find that ${haystack} ${matches}.`, () => {
+							const result = MyRegExp.testMatch(haystack, needle);
+							expect(result).to.equal(expectation);
+						});
+					}
+				});
+			});
+		}
 	});
 });
